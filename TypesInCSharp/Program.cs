18c8f8a16f8417603565638ca24d07a526974158
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2022-06-17 10:11:36 -0300

    Add project files.

---
+++ TypesInCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypesInCSharp {
+    class Program {
+
+       
+        static void Main(string[] args) {
+            // sbyte primitive data type
+            sbyte x = 100;
+
+            Console.WriteLine(x);
+        }
+    }
+}
